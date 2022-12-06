6f4c092d264b6235f1a2dada13e13e4cc38fe7f0
Mohit <mohitnandan81825@gmail.com>
2022-12-06 09:43:37 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Highlight } from "@mui/icons-material";
+
+function Header() {
+  return (
+    <header>
+    <Highlight/>
+      <h1>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
